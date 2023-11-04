1e2aeb15bb8a0e8431d07d5fec1672310e201ebe
DiegoBP02 <diegobpcelular@gmail.com>
2023-11-04 16:26:09 -0300

    feat: configurando e usando cdi como gerenciador de transacoes

---
+++ src/main/java/com/algaworks/erp/id/Pedidos.java
@@ -0,0 +1,16 @@
+package com.algaworks.erp.id;
+
+import java.math.BigDecimal;
+
+public class Pedidos {
+    
+//    @Inject
+//    private EntityManager manager;
+    
+    
+    public BigDecimal totalPedidosMesAtual() {
+        return new BigDecimal("100");
+    }
+
+}
+
