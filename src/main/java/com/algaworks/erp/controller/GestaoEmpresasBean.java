220419769d2d9e77774e45344bb8c2f05e4033e8
DiegoBP02 <diegobpcelular@gmail.com>
2023-11-06 21:48:13 -0300

    feat: listando empresas com tabela de dados

--- src/main/java/com/algaworks/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/com/algaworks/erp/controller/GestaoEmpresasBean.java
@@ -1,12 +1,15 @@
 package com.algaworks.erp.controller;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import com.algaworks.erp.model.Empresa;
 import com.algaworks.erp.model.TipoEmpresa;
+import com.algaworks.erp.repository.Empresas;
 
 @Named
 @ViewScoped
@@ -14,23 +17,17 @@ public class GestaoEmpresasBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	private Empresa empresa = new Empresa();
-
-	public void salvar() {
-		System.out.println("Razão social: " + empresa.getRazaoSocial() + " - Nome fantasia: "
-				+ empresa.getNomeFantasia() + " - Tipo: " + empresa.getTipo());
-	}
-
-	public String ajuda() {
-		return "AjudaGestaoEmpresas?faces-redirect=true";
-	}
-
-	public Empresa getEmpresa() {
-		return empresa;
-	}
-
-	public TipoEmpresa[] getTiposEmpresa() {
-		return TipoEmpresa.values();
-	}
+	@Inject
+    private Empresas empresas;
+    
+    private List<Empresa> listaEmpresas;
+    
+    public void todasEmpresas() {
+        listaEmpresas = empresas.todas();
+    }
+    
+    public List<Empresa> getListaEmpresas() {
+        return listaEmpresas;
+    }
 
 }
