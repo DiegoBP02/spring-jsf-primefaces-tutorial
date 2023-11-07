94b8aacfc8a4d75712c62505dd895b2b80964587
DiegoBP02 <diegobpcelular@gmail.com>
2023-11-07 15:01:08 -0300

    feat: implementando a pesquisa e mensagem de notificacao

--- src/main/java/com/algaworks/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/com/algaworks/erp/controller/GestaoEmpresasBean.java
@@ -8,8 +8,8 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import com.algaworks.erp.model.Empresa;
-import com.algaworks.erp.model.TipoEmpresa;
 import com.algaworks.erp.repository.Empresas;
+import com.algaworks.erp.util.FacesMessages;
 
 @Named
 @ViewScoped
@@ -20,7 +20,20 @@ public class GestaoEmpresasBean implements Serializable {
 	@Inject
     private Empresas empresas;
     
+    @Inject
+    private FacesMessages messages;
+    
     private List<Empresa> listaEmpresas;
+    
+    private String termoPesquisa;
+    
+    public void pesquisar() {
+        listaEmpresas = empresas.pesquisar(termoPesquisa);
+        
+        if (listaEmpresas.isEmpty()) {
+            messages.info("Sua consulta não retornou registros.");
+        }
+    }
     
     public void todasEmpresas() {
         listaEmpresas = empresas.todas();
@@ -30,4 +43,12 @@ public class GestaoEmpresasBean implements Serializable {
         return listaEmpresas;
     }
 
+    public String getTermoPesquisa() {
+        return termoPesquisa;
+    }
+    
+    public void setTermoPesquisa(String termoPesquisa) {
+        this.termoPesquisa = termoPesquisa;
+    }
+    
 }
